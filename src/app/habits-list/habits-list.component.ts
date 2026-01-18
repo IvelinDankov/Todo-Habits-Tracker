60b3a60be75c01409d4661fa48dd70c1be946ce2
Velin Dankov <velin80@web.de>
2026-01-18 11:45:37 +0200

    Creating starting components and models.

---
+++ src/app/habits-list/habits-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habits-list',
+  imports: [],
+  templateUrl: './habits-list.component.html',
+  styleUrl: './habits-list.component.scss'
+})
+export class HabitsListComponent {
+
+}
---
+++ src/app/stats-dashboard/stats-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-dashboard',
+  imports: [],
+  templateUrl: './stats-dashboard.component.html',
+  styleUrl: './stats-dashboard.component.scss'
+})
+export class StatsDashboardComponent {
+
+}
